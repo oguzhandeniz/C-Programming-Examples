b075f363acaf69a4e7369bf17e8ac6a68d7648b0
oguzhandeniz <oguzhandeniz2512@gmail.com>
2022-02-12 12:55:56 +0300

    Create main.c

---
+++ Loops/forLoops/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char* argv[])
+{
+
+/*
+	// For loop example
+	int i,j;
+	for (i=0; i<10; i++)
+	{
+		for (j=0; j<i; j++)
+		{
+			printf("Hello ");
+		}
+		printf("\n");
+
+	}
+*/
+
+
+
+
+/*
+
+	//Factorial Calculation
+
+	 	 i=2         Factorial=2
+		 i=3         Factorial=2*3=6
+		 i=4         Factorial=6*4=24
+		 i=5         Factorial=24*5=120
+					*
+					*
+					*
+
+
+	int i, num, factorial=1;
+	printf("Enter Number:");
+	scanf("%d",&num);
+	for (i=2; i<=num; i++)
+	{
+		factorial*=i;
+	}
+	printf("%d!=%d\n",num,factorial);
+*/
+
+
+
+
+/*
+	// Find the exponent of numbers
+	int i, j;
+	for (i=2; i<=10; i++)
+	{
+		for (j=1; j<10; j++)
+		{
+			int result=pow(i, j);
+			printf("%d ",result);
+		}
+		printf("\n");
+	}
+*/
+	return 0;
+}
